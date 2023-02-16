897d2db8b2a282c2a061e21f8c5b5df2a2be05fa
UP210586 <up210586@alumnos.upa.edu.mx>
2023-02-15 23:37:39 -0600

    15/02

--- Unit 2/Posfix.py
+++ Unit 2/Posfix.py
@@ -1,4 +1,28 @@
-import os
-import math 
-from math import log10
-from math import log
+def prioridad(c):
+    if c in ["+", "-"]:
+        return int(1)
+    elif c in ["*", "/"]:
+        return int(2)
+    elif c in ["^"]:
+        return int(3)
+    elif c in ["(", ")"]:
+        return int(0)
+
+
+op = {"+", "-", "/", "*", "^"}
+Q = ["5", "*", "(", "6", "+", "2", ")", "-", "12", "/", "4"]
+S = []
+P = []
+
+Q.insert(0, "(")
+Q.append(")")
+for i in Q:
+    if i != "(" and i != ")" and i not in op:
+        P.append(i)
+    elif i == "(":
+        S.append(i)
+    elif i in op:
+        for j in reversed(S):
+            a = prioridad(i)
+            b = prioridad(j)
+            print(a, b)
